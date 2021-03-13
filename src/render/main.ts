b234f8ebb199c92410391f7fa26e3a41e058246b
草鞋没号 <308487730@qq.com>
2021-03-13 22:15:35 +0800

    chore: UPDATE

--- src/render/main.ts
+++ src/render/main.ts
@@ -4,9 +4,10 @@ import { createApp } from 'vue'
 import App from './App'
 import router from './router'
 
+// vite 会编译 import 的形式；所以 electron 及 node.js 内置模块用 require 形式
 const os = require('os')
 const fs = require('fs')
-const { ipcRenderer } = require('electron') // vite 会编译 import 的形式；所以 electron 及 node.js 内置模块用 require 形式
+const { ipcRenderer } = require('electron')
 const Store = require('electron-store')
 
 // 开发期全部引入 antd.css；打包期用 babel-plugin-import 按需引入样式
