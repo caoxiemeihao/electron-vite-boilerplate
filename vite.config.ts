2ce9a96cd4286e4f77ffec3c31d822fa1df5e901
草鞋没号 <308487730@qq.com>
2021-05-23 16:44:17 +0800

    feat: use vitejs-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,18 @@ import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { esm2cjs } from './script/plugins'
-import pkg from './package.json'
+import electron from 'vitejs-plugin-electron'
 
-export default defineConfig(({ /* development | production */mode, command }) => ({
+export default defineConfig(({ /* development | production */mode }) => ({
   plugins: [
     vue(),
     vueJsx(), // Auto inject "import { h } from 'vue'"
-    command === 'serve' && esm2cjs(Object.keys(pkg.devDependencies)),
+    electron(),
   ].filter(Boolean),
   root: join(__dirname, 'src/render'),
   base: './', // index.html 中静态资源加载位置
   server: {
     port: +process.env.PORT,
-  },
-  optimizeDeps: {
-    exclude: [...Object.keys(pkg.devDependencies)],
   },
   resolve: {
     alias: {
