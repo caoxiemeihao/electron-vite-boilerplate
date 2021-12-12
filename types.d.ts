364dc9c8d8525defd310f6a4c4413e51c30748bf
草鞋没号 <308487730@qq.com>
2021-12-12 17:02:42 +0800

    chore: UPDATE

--- types.d.ts
+++ types.d.ts
@@ -5,3 +5,9 @@ interface Window {
   /** 运行时环境 */
   APP_ENV: 'development' | 'production'
 }
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production'
+  }
+}
