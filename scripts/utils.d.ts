3170aeb08c9bebacabbcc69c6cdd8251c3dba972
草鞋没号 <308487730@qq.com>
2021-12-20 10:43:17 +0800

    allows js in typescript

--- scripts/utils.d.ts
+++
@@ -1,6 +0,0 @@
-import { Compiler } from 'webpack'
-
-export type CallbackFunction = (tag: string, cb?: (bool: boolean) => void)
-  => Parameters<Compiler['run']>[0]
-
-export const callbackFunction: CallbackFunction
