6ad4db7c3c010454160349f720789f22edd73fc4
草鞋没号 <308487730@qq.com>
2022-01-15 14:44:37 +0800

    watch use vite

--- scripts/watch.mjs
+++ scripts/watch.mjs
@@ -1,41 +1,48 @@
 import electron from 'electron';
 import cp from 'child_process';
 import { createRequire } from 'module';
-import esbuild from 'esbuild';
-import vite from 'vite';
-import { mainOptions, preloadOptions } from './esbuild.mjs';
+import { createServer, build } from 'vite';
+import { main, preload } from './vite.config.mjs';
 const pkg = createRequire(import.meta.url)('../package.json');
+
+/**
+ * @param {import('vite').ViteDevServer} server 
+ */
+function watchPreload(server) {
+  preload.build.watch = true;
+  preload.plugins = [{
+    name: 'electron-preload-watcher',
+    writeBundle() {
+      server.ws.send({ type: 'full-reload' });
+    },
+  }].concat(preload.plugins).filter(Boolean);
+
+  return build(preload);
+}
 
 function watchMain() {
   /**
    * @type {import('child_process').ChildProcessWithoutNullStreams | null}
    */
   let electronProcess = null;
-  const startElectron = () => {
-    electronProcess && electronProcess.kill();
-    electronProcess = cp.spawn(electron, ['.'], {
-    env: Object.assign(process.env, pkg.env),
-      stdio: 'inherit',
-    });
-  };
 
-  esbuild.build(mainOptions(startElectron));
-  startElectron();
-}
+  main.build.watch = true;
+  main.plugins = [{
+    name: 'electron-main-watcher',
+    writeBundle() {
+      electronProcess && electronProcess.kill();
+      electronProcess = cp.spawn(electron, ['.'], {
+        env: Object.assign(process.env, pkg.env),
+        stdio: 'inherit',
+      });
+    },
+  }].concat(main.plugins).filter(Boolean);
 
-/**
- * @param {import('vite').ViteDevServer} server 
- */
-function watchPreload(server) {
-  esbuild.build(preloadOptions(() => {
-    server.ws.send({ type: 'full-reload' });
-  }));
+  return build(main);
 }
 
 // bootstrap
-(async () => {
-  const server = await vite.createServer({ configFile: 'src/renderer/vite.config.ts' })
-  await server.listen(pkg.env.PORT);
-  watchPreload(server);
-  watchMain();
-})();
+const server = await createServer({ configFile: 'src/renderer/vite.config.ts' });
+await server.listen(pkg.env.PORT);
+await watchPreload(server);
+await watchMain();
