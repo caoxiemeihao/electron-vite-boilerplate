2924c2547d86a793d487268a29ef049fabc4eab1
草鞋没号 <308487730@qq.com>
2022-02-26 10:09:33 +0800

    Beautify layout

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,13 +1,27 @@
 import './samples/nodejs-api'
 import './samples/electron-store'
 import './samples/serialport'
+import electron from './assets/electron.png'
+import vite from './assets/vite.svg'
+import ts from './assets/ts.svg'
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+app.innerHTML = renderHTML()
 
 window.removeLoading()
+
+function renderHTML() {
+  return `
+  <div class="logo-box">
+    <img src="${electron}" />
+    <img src="${vite}" />
+    <img src="${ts}" />
+  </div>
+  <div class="static-public">
+    Place static files into the <code>src/renderer/public</code> folder
+    <img src="/images/node.png" />
+  </div>
+`
+}
