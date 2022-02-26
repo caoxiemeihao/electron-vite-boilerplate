7126c9ebc88ebc7064e796c1c26e3b1a413170ee
草鞋没号 <308487730@qq.com>
2022-02-26 08:50:30 +0800

    remove vue @vuejs/plugin-vue

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,6 +1,5 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import electronRenderer from 'vite-plugin-electron-renderer'
 import pkg from '../../package.json'
 
@@ -9,7 +8,6 @@ export default defineConfig({
   root: __dirname,
   mode: process.env.NODE_ENV,
   plugins: [
-    vue(),
     electronRenderer({
       resolve: {
         'electron-store': 'export default require("electron-store");',
