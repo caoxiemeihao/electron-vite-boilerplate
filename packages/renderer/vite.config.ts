8ce79b3a57fddea58ff80f2084386bb663705db5
草鞋没号 <308487730@qq.com>
2022-03-22 11:37:50 +0800

    remove vite-plugin-electron-renderer

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,20 +1,11 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
-import electronRenderer from 'vite-plugin-electron-renderer'
 import pkg from '../../package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: __dirname,
   mode: process.env.NODE_ENV,
-  plugins: [
-    electronRenderer({
-      resolve: {
-        sqlite3: 'export default require("sqlite3");',
-        serialport: 'export default require("serialport");',
-      },
-    }),
-  ],
   build: {
     emptyOutDir: true,
     outDir: '../../dist/renderer',
