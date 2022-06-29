797cabf1ba0c7a6416d5d2e91d6cc834eb1b84c2
草鞋没号 <308487730@qq.com>
2022-06-29 13:56:19 +0800

    remove `samples` in Electron-Renderer

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,21 +1,9 @@
-import renderNodejsApi from '@/samples/nodejs-api'
-import renderSerialport from '@/samples/serialport'
-import renderSqlite3 from '@/samples/sqlite3'
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = renderHTML()
-
-window.removeLoading()
-
-function renderHTML() {
-  return `
-  <div class="logo-box">
-    <img height="124" src="./logo.svg" />
-    ${renderNodejsApi()}
-    ${renderSerialport()}
-    ${renderSqlite3()}
-  </div>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div class="logo-box">
+  <img height="470" src="./logo.svg" />
+</div>
 `
-}
+
+postMessage({ payload: 'removeLoading' }, '*')
